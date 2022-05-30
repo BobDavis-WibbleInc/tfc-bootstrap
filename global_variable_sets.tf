681cd292b36713210d426b7d244c8adb274d2fd8
Bob Davis <bob@wibble.net.nz>
2022-05-30 17:01:39 +1200

    change to gloal vars

--- global_variable_sets.tf
+++ global_variable_sets.tf
@@ -12,13 +12,11 @@ resource "tfe_variable_set" "global_variables" {
 resource "tfe_variable" "az_vars" {
   key = "az_vars"
   value = replace(jsonencode({
-    az_vars = {
-      client_secret   = var.az_client_secret
-      client_id       = var.az_client_id
-      tenant_id       = var.az_tenant_id
-      subscription_id = var.az_subscription_id
-      display_name    = var.az_display_name
-    }
+    client_secret   = var.az_client_secret
+    client_id       = var.az_client_id
+    tenant_id       = var.az_tenant_id
+    subscription_id = var.az_subscription_id
+    display_name    = var.az_display_name
   }), "/(\".*?\"):/", "$1 = ")
   hcl             = true
   sensitive       = true
@@ -30,15 +28,13 @@ resource "tfe_variable" "az_vars" {
 resource "tfe_variable" "tf_vars" {
   key = "tf_vars"
   value = replace(jsonencode({
-    tf_vars = {
-      org                 = "wibble"
-      admin_team          = "bob"
-      version             = "1.2.1"
-      auto_apply          = true
-      execution_mode      = "remote"
-      global_remote_state = true
-      vcs_repo            = true
-    }
+    org                 = "wibble"
+    admin_team          = "bob"
+    version             = "1.2.1"
+    auto_apply          = true
+    execution_mode      = "remote"
+    global_remote_state = true
+    vcs_repo            = true
   }), "/(\".*?\"):/", "$1 = ")
   hcl             = true
   sensitive       = false
