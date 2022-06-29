7f078453288e4fab7584114f4ecb17d9b546d085
Bob Davis <bob@wibble.net.nz>
2022-06-29 16:03:35 +1200

    removing sentinel policies

--- sentinel.tf
+++ sentinel.tf
@@ -1,22 +1,22 @@
 #----------------------------------------------------------------------------------------#
 # Create Sentinel Azure Policy Set
 #----------------------------------------------------------------------------------------#
-data "tfe_workspace_ids" "azure-workspaces" {
-  tag_names    = ["azure"]
-  organization = "wibble"
-}
+# data "tfe_workspace_ids" "azure-workspaces" {
+#   tag_names    = ["azure"]
+#   organization = "wibble"
+# }
 
-resource "tfe_policy_set" "azure-policies" {
-  name          = "azure-policy-set"
-  description   = "Policy Set to Hold for Azure Related Policies"
-  organization  = "wibble"
-  policies_path = "policies/azure"
-  workspace_ids = values(data.tfe_workspace_ids.azure-workspaces.ids)
+# resource "tfe_policy_set" "azure-policies" {
+#   name          = "azure-policy-set"
+#   description   = "Policy Set to Hold for Azure Related Policies"
+#   organization  = "wibble"
+#   policies_path = "policies/azure"
+#   workspace_ids = values(data.tfe_workspace_ids.azure-workspaces.ids)
 
-  vcs_repo {
-    identifier         = "BobDavis-WibbleInc/tfc-bootstrap"
-    oauth_token_id     = "ot-6172QnkrWpoNQG87"
-    branch             = "main"
-    ingress_submodules = false
-  }
-}
+#   vcs_repo {
+#     identifier         = "BobDavis-WibbleInc/tfc-bootstrap"
+#     oauth_token_id     = "ot-6172QnkrWpoNQG87"
+#     branch             = "main"
+#     ingress_submodules = false
+#   }
+# }
