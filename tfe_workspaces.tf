4e4913b3cb0d5e7bbe601b9f5224c6cf7f0bdeba
Bob Davis <bob@wibble.net.nz>
2022-07-01 18:16:47 +1200

    done for the day

--- tfe_workspaces.tf
+++ tfe_workspaces.tf
@@ -1,44 +1,44 @@
-resource "tfe_workspace" "tfc_azure" {
-  name               = "tfc-azure"
-  organization       = "wibble"
-  allow_destroy_plan = true
-  auto_apply         = true
+# resource "tfe_workspace" "tfc_azure" {
+#   name               = "tfc-azure"
+#   organization       = "wibble"
+#   allow_destroy_plan = true
+#   auto_apply         = true
 
-  vcs_repo {
-    identifier     = "BobDavis-WibbleInc/tfc-azure"
-    oauth_token_id = "ot-6172QnkrWpoNQG87"
-    branch         = "main"
-  }
+#   vcs_repo {
+#     identifier     = "BobDavis-WibbleInc/tfc-azure"
+#     oauth_token_id = "ot-6172QnkrWpoNQG87"
+#     branch         = "main"
+#   }
 
-  tag_names = ["azure"]
-}
+#   tag_names = ["azure"]
+# }
 
-resource "tfe_workspace" "tfc_aws" {
-  name               = "tfc-aws"
-  organization       = "wibble"
-  allow_destroy_plan = true
-  auto_apply         = true
+# resource "tfe_workspace" "tfc_aws" {
+#   name               = "tfc-aws"
+#   organization       = "wibble"
+#   allow_destroy_plan = true
+#   auto_apply         = true
 
-  vcs_repo {
-    identifier     = "BobDavis-WibbleInc/tfc-aws"
-    oauth_token_id = "ot-6172QnkrWpoNQG87"
-    branch         = "main"
-  }
+#   vcs_repo {
+#     identifier     = "BobDavis-WibbleInc/tfc-aws"
+#     oauth_token_id = "ot-6172QnkrWpoNQG87"
+#     branch         = "main"
+#   }
 
-  tag_names = ["aws"]
-}
+#   tag_names = ["aws"]
+# }
 
 
-resource "tfe_workspace" "tfc_devops" {
-  name               = "tfc-devops"
-  organization       = "wibble"
-  allow_destroy_plan = true
-  auto_apply         = true
+# resource "tfe_workspace" "tfc_devops" {
+#   name               = "tfc-devops"
+#   organization       = "wibble"
+#   allow_destroy_plan = true
+#   auto_apply         = true
 
-  vcs_repo {
-    identifier     = "BobDavis-WibbleInc/tfc-devops"
-    oauth_token_id = "ot-6172QnkrWpoNQG87"
-    branch         = "main"
-  }
-  tag_names = ["azure", "devops"]
-}
+#   vcs_repo {
+#     identifier     = "BobDavis-WibbleInc/tfc-devops"
+#     oauth_token_id = "ot-6172QnkrWpoNQG87"
+#     branch         = "main"
+#   }
+#   tag_names = ["azure", "devops"]
+# }
