e64e63c5ed82477a258bf337f6adac28ec1e5318
Bob Davis <bob@wibble.net.nz>
2022-07-04 15:56:39 +1200

    fire it backup

--- provider.tf
+++ provider.tf
@@ -12,18 +12,6 @@ terraform {
       source  = "hashicorp/tfe"
       version = "~> 0.32.1"
     }
-    # azuread = {
-    #   source  = "hashicorp/azuread"
-    #   version = "~> 2.11.0"
-    # }
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = "~> 2.90.0"
-    # }
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "~> 3.70.0"
-    # }
   }
 }
 
@@ -31,49 +19,3 @@ provider "tfe" {
   # Configuration options
   token = var.tf_token
 }
-
-# provider "azuredevops" {
-#   # Configuration options
-#   org_service_url       = "https://dev.azure.com/${var.azuredevops_settings.organization}"
-#   personal_access_token = var.azuredevops_personal_token
-# }
-
-# provider "azuread" {
-#   # Configuration options
-#   tenant_id     = var.azuread_settings.tenant_id
-#   client_id     = var.azuread_settings.client_id
-#   client_secret = var.azuread_client_secret
-# }
-
-# provider "azurerm" {
-#   # Configuration options
-#   features {
-#     key_vault {
-#       recover_soft_deleted_key_vaults = false
-#       purge_soft_delete_on_destroy    = false
-#     }
-#   }
-
-#   tenant_id       = var.azuread_settings.tenant_id
-#   client_id       = var.azuread_settings.client_id
-#   object_id       = var.azurerm_settings.object_id
-#   client_secret   = var.azuread_client_secret
-#   subscription_id = var.azurerm_settings.bootstrap_subscription_id
-# }
-
-# provider "aws" {
-#   # Configuration options
-#   region     = var.aws_settings.aws_region
-#   access_key = var.aws_settings.aws_access_key
-#   secret_key = var.aws_secret_key
-# }
-
-
-
-# backend "remote" {
-#   hostname     = "app.terraform.io"
-#   organization = "wibble"
-
-#   workspaces {
-#     name = "tfc-bootstrap"
-#   }
--- tfe_workspaces.tf
+++ tfe_workspaces.tf
@@ -1,17 +1,17 @@
-# resource "tfe_workspace" "tfc_azure" {
-#   name               = "tfc-azure"
-#   organization       = "wibble"
-#   allow_destroy_plan = true
-#   auto_apply         = true
+resource "tfe_workspace" "tfc_azure" {
+  name               = "tfc-azure"
+  organization       = "wibble"
+  allow_destroy_plan = true
+  auto_apply         = true
 
-#   vcs_repo {
-#     identifier     = "BobDavis-WibbleInc/tfc-azure"
-#     oauth_token_id = "ot-6172QnkrWpoNQG87"
-#     branch         = "main"
-#   }
+  vcs_repo {
+    identifier     = "BobDavis-WibbleInc/tfc-azure"
+    oauth_token_id = "ot-6172QnkrWpoNQG87"
+    branch         = "main"
+  }
 
-#   tag_names = ["azure"]
-# }
+  tag_names = ["azure"]
+}
 
 # resource "tfe_workspace" "tfc_aws" {
 #   name               = "tfc-aws"
